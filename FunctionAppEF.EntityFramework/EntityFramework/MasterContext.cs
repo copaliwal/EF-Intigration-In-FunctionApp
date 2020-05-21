3cff4d8af6178a6dd8184186701a2a0d1d602e5f
Chandrashekhar Paliwal <copaliwal@gmail.com>
2020-05-21 22:06:17 +0530

    Added Master Context class and one model class

---
+++ FunctionAppEF.EntityFramework/EntityFramework/MasterContext.cs
@@ -0,0 +1,15 @@
+﻿using FunctionAppEF.EntityFramework.EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FunctionAppEF.EntityFramework.EntityFramework
+{
+    public class MasterContext : DbContext
+    {
+        public MasterContext(DbContextOptions<MasterContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MasterOrganization> Organizations { get; set; }
+    }
+}
