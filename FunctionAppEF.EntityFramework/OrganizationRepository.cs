882c42e65d8d01c1dd38366caf9769e1ba247124
Chandrashekhar Paliwal <copaliwal@gmail.com>
2020-05-21 23:18:22 +0530

    Added Repository Class fo db oeration

---
+++ FunctionAppEF.EntityFramework/OrganizationRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FunctionAppEF.EntityFramework.EntityFramework;
+using FunctionAppEF.EntityFramework.EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FunctionAppEF.EntityFramework
+{
+    public interface IOrganizationRepository
+    {
+        Task<string> GetOrganisationConnection();
+    }
+
+    public class OrganizationRepository : IOrganizationRepository
+    {
+        private readonly MasterContext _masterContext;
+
+        public OrganizationRepository(
+            MasterContext masterContext)
+        {
+            _masterContext = masterContext;
+        }
+
+        public async Task<string> GetOrganisationConnection()
+        {
+            string name = "CP001";
+            var organization = await _masterContext.MasterOrganizations
+                .Where(o => o.Name == name)
+                .FirstOrDefaultAsync();
+
+            if (organization == null)
+                return $"There is no organization with tenant key {name}";
+
+            return organization.Connections;
+        }
+    }
+}
